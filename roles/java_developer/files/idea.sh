07581fec5248dda5b025f4094cb79e8c13a6104b
Marek Lesiewski <marek.lesiewski@gmail.com>
2020-02-06 15:09:34 +0100

    small improvements

---
+++ roles/java_developer/files/idea.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+/opt/idea/idea-IU-193.6015.39/bin/idea.sh
--- run_playbook.sh
+++ run_playbook.sh
@@ -4,6 +4,6 @@ echo -n sudo Password:
 read -s szPassword
 
 echo $szPassword | sudo -S pacmatic -Syu
-yay -Syu
+echo $szPassword | sudo -S yay -Syu
 
 echo $szPassword | sudo -S ansible-playbook -i $1_inv.yml $1.yml --extra-vars "ansible_become_password=$szPassword"
