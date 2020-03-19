4151214ed584ceaab388f757cb9c73ef1af9b7dc
Marek Lesiewski <marek.lesiewski@gmail.com>
2020-03-19 11:53:04 +0100

    imrpove create_role.sh
    * doublequote variables
    * remove 'tasks' key as it was an error to have it there

--- create_role.sh
+++ create_role.sh
@@ -3,7 +3,6 @@
 MAIN_DIR=roles/$1/tasks
 MAIN_FILE=$MAIN_DIR/main.yml
 
-mkdir -p $MAIN_DIR
-touch $MAIN_FILE
-echo "# $1 - $2" >> $MAIN_FILE
-echo "tasks:" >> $MAIN_FILE
+mkdir -p "$MAIN_DIR"
+touch "$MAIN_FILE"
+echo "# $1 - $2" >> "$MAIN_FILE"
