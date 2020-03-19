02ce078caa06ab1289291fc6d506ab497cbcea03
Marek Lesiewski <tirpitz-verus@gmail.com>
2020-03-19 15:17:31 +0100

    refactor run_playbook script
    - remove upgreade commands
    - validate that system is up to date

--- run_playbook.sh
+++ run_playbook.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 
 echo -n sudo Password: 
-read -s szPassword
+read -sr szPassword
+echo ""
 
-echo $szPassword | sudo -S pacmatic -Syu
-echo $szPassword | sudo -S yay -Syu
+if echo "$szPassword" | sudo -S pacman -Syup | grep -q 'https';
+then
+  echo "!! system needs an upgreade"
+  echo "please run:"
+  echo "  sudo pacmatic -Syu && yay -Syu"
+  exit 1
+else
+  echo "system up to date"
+fi
 
-echo $szPassword | sudo -S ansible-playbook -i $1_inv.yml $1.yml --extra-vars "ansible_become_password=$szPassword"
+echo "$szPassword" | sudo -S ansible-playbook -i "$1_inv.yml" "$1.yml" --extra-vars "ansible_become_password=$szPassword"
