fa738d88808e7acafc69b68de1f7a5c17be16d10
tirpitz-verus <tirpitz-verus@gmail.com>
2023-02-25 20:36:36 +0100

    add dhcpcd to initial setup

--- initial_setup.sh
+++ initial_setup.sh
@@ -2,7 +2,7 @@
 
 # install software required to run ansible
 pacman -Syu
-pacman -S vim ansible git screen tree man go base-devel netctl
+pacman -S vim ansible git screen tree man go base-devel netctl dhcpcd
 
 # setup aur_builder user
 if ! id -u aur_builder > /dev/null 2>&1
